e482aa6fdbf481388a234bdccbad69bcec47de78
schengawegga <schengawegga@gmail.com>
2023-11-05 00:24:26 +0100

    task: trigger deprecation warning on LOGIN, CRAM-MD5 and DIGEST-MD5

--- Auth/SASL.php
+++ Auth/SASL.php
@@ -69,10 +69,9 @@ class Auth_SASL
                 break;
 
             case 'login':
-                /* TODO trigger deprecation warning in 2.0.0 and remove LOGIN authentication in 3.0.0
+                // TODO remove LOGIN authentication in 3.0.0
                 trigger_error(__CLASS__ . ': Authentication method LOGIN' .
                     ' is no longer secure and should be avoided.', E_USER_DEPRECATED);
-                */
                 $filename  = 'Auth/SASL/Login.php';
                 $classname = 'Auth_SASL_Login';
                 break;
@@ -91,10 +90,9 @@ class Auth_SASL
                 // $msg = 'Deprecated mechanism name. Use IANA-registered name: CRAM-MD5.';
                 // trigger_error($msg, E_USER_DEPRECATED);
             case 'cram-md5':
-                /* TODO trigger deprecation warning in 2.0.0 and remove CRAM-MD5 authentication in 3.0.0
+                // TODO remove CRAM-MD5 authentication in 3.0.0
                 trigger_error(__CLASS__ . ': Authentication method CRAM-MD5' .
                     ' is no longer secure and should be avoided.', E_USER_DEPRECATED);
-                */
                 $filename  = 'Auth/SASL/CramMD5.php';
                 $classname = 'Auth_SASL_CramMD5';
                 break;
@@ -103,10 +101,9 @@ class Auth_SASL
                 // $msg = 'Deprecated mechanism name. Use IANA-registered name: DIGEST-MD5.';
                 // trigger_error($msg, E_USER_DEPRECATED);
             case 'digest-md5':
-                /* TODO trigger deprecation warning in 2.0.0 and remove DIGEST-MD5 authentication in 3.0.0
+                // TODO remove DIGEST-MD5 authentication in 3.0.0
                 trigger_error(__CLASS__ . ': Authentication method DIGEST-MD5' .
                     ' is no longer secure and should be avoided.', E_USER_DEPRECATED);
-                */
                 $filename  = 'Auth/SASL/DigestMD5.php';
                 $classname = 'Auth_SASL_DigestMD5';
                 break;
